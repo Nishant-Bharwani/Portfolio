a8618acd9bfd89f0503a23807df65aec75277acb
Nishant-Bharwani <nbtaylor1031@gmail.com>
2024-01-19 21:19:17 +0530

    Twitter handle corrected

--- src/data/SocialMediaLinks.js
+++ src/data/SocialMediaLinks.js
@@ -7,25 +7,25 @@ import {
 } from "react-icons/si";
 
 export const SocialMediaLinks = [{
-        icon: SiGmail,
-        url: "mailto:nbtaylor1031@gmail.com",
-    },
-    {
-        icon: SiGithub,
-        url: "https://github.com/Nishant-Bharwani",
-    },
+    icon: SiGmail,
+    url: "mailto:nbtaylor1031@gmail.com",
+},
+{
+    icon: SiGithub,
+    url: "https://github.com/Nishant-Bharwani",
+},
 
-    {
-        icon: SiTwitter,
-        url: "https://twitter.com/NishantBharwan3",
-    },
-    {
-        icon: SiLinkedin,
-        url: "https://linkedin.com/in/nishant-bharwani",
-    },
-    {
-        icon: SiInstagram,
-        url: "https://instagram.com/bharwaninishant",
-    },
+{
+    icon: SiTwitter,
+    url: "https://twitter.com/Nishant1031",
+},
+{
+    icon: SiLinkedin,
+    url: "https://linkedin.com/in/nishant-bharwani",
+},
+{
+    icon: SiInstagram,
+    url: "https://instagram.com/bharwaninishant",
+},
 
 ];
